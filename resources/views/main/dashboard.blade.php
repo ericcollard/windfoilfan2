e8cd23262bd3a0d85e58f0e65bcd9b420799b44b
eric <eric.collard@free.fr>
2022-03-14 15:36:25 +0100

    Mise à jour du tableau de bord marque

--- resources/views/main/dashboard.blade.php
+++ resources/views/main/dashboard.blade.php
@@ -16,14 +16,6 @@
         <div class="col-12">
             <div class="page-title-box">
                 <div class="page-title-right">
-                    <form class="d-flex">
-                        <div class="input-group">
-                            <input type="text" class="form-control form-control-light" id="dash-daterange">
-                            <span class="input-group-text bg-primary border-primary text-white">
-                                <i class="mdi mdi-calendar-range font-13"></i>
-                            </span>
-                        </div>
-                    </form>
                 </div>
                 <h4 class="page-title">{{ __('Dashboard') }}</h4>
             </div>
